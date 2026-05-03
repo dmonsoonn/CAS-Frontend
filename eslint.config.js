473a65b6c97c2cce69fac100601d39213ec1ad93
Mokrov Danila <mok-danil28@mail.ru>
2026-05-03 13:46:05 +0300

    fix: config files

--- eslint.config.js
+++ eslint.config.js
@@ -6,13 +6,19 @@ import tseslint from 'typescript-eslint'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
 export default defineConfig([
-  globalIgnores(['dist']),
+  globalIgnores([
+    'dist',
+    'build',
+    'node_modules',
+    '*.config.js',
+    '*.config.ts',
+  ]),
   {
     files: ['**/*.{ts,tsx}'],
     extends: [
       js.configs.recommended,
       tseslint.configs.recommended,
-      reactHooks.configs['recommended-latest'],
+      reactHooks.configs.flat['recommended-latest'],
       reactRefresh.configs.vite,
     ],
     languageOptions: {
